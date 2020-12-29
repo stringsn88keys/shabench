806618df8125e8e785724f568a71009ca48cd33b
Thomas Powell <twilliampowell@gmail.com>
2020-12-29 09:20:12 -0600

    Refactored single and re-ran some environments

--- bench.rb
+++ bench.rb
@@ -5,21 +5,35 @@ require 'benchmark'
 require 'digest'
 
 INITIAL='Message'
+PREHASHFORMAT="#{INITIAL}%010d"
+THREADS=ENV['THREADS'] || 4
+THREADITERATIONS=ENV['THREADITERATIONS'] || 1000
+SINGLE_TEST_VALUE='ROW'
 
 def bench_single
-    times = Benchmark.realtime do
-      counter = 0
-      @source = ''
-      @target = ''
-      while true
-        @source="#{INITIAL}%010d" % counter
-        @digest=Digest::SHA512.base64digest(@source)
-        break if @digest.start_with?('ROW')
-        counter += 1
-      end
-
+  run_time = Benchmark.realtime do
+    counter = 0
+    @source = ''
+    @target = ''
+    while true
+      @source=PREHASHFORMAT % counter
+      @digest=Digest::SHA512.base64digest(@source)
+      break if @digest.start_with?(SINGLE_TEST_VALUE)
+      counter += 1
     end
-    {source: @source, digest: @digest, times: times}
+
+  end
+  {source: @source, digest: @digest, time: run_time}
+end
+
+def bench_multithread
+  run_time = Benchmark.realtime do
+    counter = 0 
+    (0...THREADS).each do
+      Thread.new { Thread.current[:output] = check_hashes(counter, THREADITERATIONS, )}
+      counter += THREADITERATIONS
+    end
+  end
 end
 
 def lambda_handler(event:, context:)
